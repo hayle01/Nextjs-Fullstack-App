149b63fc3807f249a5a0cfa03e87f670f9019a2a
hayle01 <mabdirahim832@gmail.com>
2025-11-09 21:46:03 +0300

    Some style

--- app/dashboard/user/(pages)/projects/page.tsx
+++ app/dashboard/user/(pages)/projects/page.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const ProjectsPage = () => {
   return (
-    <div>ProjectsPage
-        <h1>Hello, Projects</h1>
+    <div className='h-screen'>
+        <div className='flex items-center justify-between space-x-2'>
+            <p>This is the projects page</p>
+        </div>
+        
     </div>
   )
 }
