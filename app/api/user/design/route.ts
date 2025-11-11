1f694ef220f352512346a93c084d098d4bca8a3f
hayle01 <mabdirahim832@gmail.com>
2025-11-11 22:26:36 +0300

    Create POST

--- app/api/user/design/route.ts
+++ app/api/user/design/route.ts
@@ -1,6 +1,18 @@
 import openAi from 'openai';
 import { AuthOptions } from '../../auth/[...nextauth]/authOptions';
+import { NextRequest, NextResponse } from 'next/server';
+import { getServerSession } from 'next-auth';
+
 
 const openAI =  new openAi({
     apiKey: process.env.OPENAI_API_KEY
 })
+
+export const POST = async (request: NextRequest) => {
+    const session = await getServerSession(AuthOptions);
+    if(!session || !session.user){
+        return NextResponse.json("unauthorized access please login", {status: 403});
+    }
+    // TODO: CREATE POST 
+    
+}
