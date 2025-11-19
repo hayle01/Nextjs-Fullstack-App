3c6a48fa5cf142146527f0e105cc534102bd7776
hayle01 <mabdirahim832@gmail.com>
2025-11-19 16:04:31 +0300

    demo chnages and

--- app/api/auth/[...nextauth]/authOptions.ts
+++ app/api/auth/[...nextauth]/authOptions.ts
@@ -1,54 +1,54 @@
 import { NextAuthOptions } from "next-auth";
 import prisma from "../../../../prisma/client";
-import  GoogleProvider  from 'next-auth/providers/google'
-import  GithubProvider  from 'next-auth/providers/github'
-import  FacebookProvider  from 'next-auth/providers/facebook'
-import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
+import FacebookProvider from "next-auth/providers/facebook";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 export const AuthOptions: NextAuthOptions = {
-    secret: process.env.NEXTAUTH_SECRET,
-    providers: [
-        GoogleProvider({
-            clientId : process.env.GOOGLE_Client_ID as string,
-            clientSecret : process.env.GOOGLE_CLIENT_SECRET as string
-        }),
-        GithubProvider({
-            clientId : process.env.GOOGLE_Client_ID as string,
-            clientSecret : process.env.GOOGLE_CLIENT_SECRET as string
-        }),
-        FacebookProvider({
-            clientId : process.env.GOOGLE_Client_ID as string,
-            clientSecret : process.env.GOOGLE_CLIENT_SECRET as string
-        }),
-    ],
-   callbacks: {
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_Client_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    GithubProvider({
+      clientId: process.env.GOOGLE_Client_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    FacebookProvider({
+      clientId: process.env.GOOGLE_Client_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  callbacks: {
     jwt: async ({ token }) => {
       const userInfo = await prisma.user.findUnique({
         where: { email: token.email as string },
       });
 
       if (userInfo) {
-        userInfo.emailVerified = undefined!; 
+        userInfo.emailVerified = undefined!;
         token.user = userInfo!;
-       //token.user = {
+        //token.user = {
         //   id: userInfo.id,
         //   name: userInfo.name,
         //   email: userInfo.email,
         //   image: userInfo.image,
         //   role: userInfo.role,
         //   credit: userInfo.credit,
-       // };
+        // };
       }
 
       return token;
     },
 
-    session: async({ session, token }) => {
+    session: async ({ session, token }) => {
       if (token.user) session.user = token.user!;
       return session;
     },
   },
-    session: {
-        strategy: "jwt"
-    },
-    adapter: PrismaAdapter(prisma)
-}
+  session: {
+    strategy: "jwt",
+  },
+  adapter: PrismaAdapter(prisma),
+};
--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -1,13 +1,10 @@
 "use client";
-import React, { useState } from "react";
 import { Sidebar, SidebarBody, SidebarLink } from "@/components/ui/sidebar";
-import { LayoutDashboard, UserCog, Settings, LogOut } from "lucide-react";
-import Link from "next/link";
-import { motion } from "framer-motion";
-import Image from "next/image";
 import { cn } from "@/lib/utils";
-
-
+import { motion } from "framer-motion";
+import { LayoutDashboard, LogOut, Settings, User, UserCog } from "lucide-react";
+import Link from "next/link";
+import { useState } from "react";
 function SidebarDemo() {
    
   const links = [
@@ -44,7 +41,7 @@ function SidebarDemo() {
   return (
     <div
       className={cn(
-        "flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-full flex-1 max-w-7xl mx-auto border border-neutral-200 dark:border-neutral-700 overflow-hidden",
+        "flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-full flex-1 max-w-7xl mx-auto  overflow-hidden",
         "h-screen"
       )}
     >
@@ -64,14 +61,8 @@ function SidebarDemo() {
                 label: "moabdirahim",
                 href: "#",
                 icon: (
-                  <Image
-                    src=""
-                    className="h-7 w-7 flex-shrink-0 rounded-full"
-                    width={50}
-                    height={50}
-                    alt="Avatar"
-                  />
-                ),
+                 <UserCog className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" /> 
+                )
               }}
             />
           </div>
@@ -115,7 +106,7 @@ export const LogoIcon = () => {
 const Dashboard = () => {
   return (
     <div className="flex flex-1">
-      <div className="p-2 md:p-10  border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
+      <div className="p-2 md:p-6  border-l border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
         <div className="flex gap-2">
           {[...new Array(4)].map((i) => (
             <div
